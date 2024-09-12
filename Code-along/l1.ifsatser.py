5f3904d50e7abf4c5fc4e8dfad849e8a0e908337
Ludwilton <ludwig.carlegrund@gmail.com>
2024-09-12 10:55:12 +0200

    alla exercises + code-alongs tillagt

---
+++ Code-along/l1.ifsatser.py
@@ -0,0 +1,59 @@
+# age = int(input("enter your age: "))
+# age_limit = 15
+# 
+# if age > age_limit:
+#     print("du får se filmen")
+# else:
+#     print("åldersgräns är 15 år")
+# 
+# 
+# number = 5
+# if number % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+# 
+# weight = float(input("input your weight (kg)"))
+# if weight < 20:
+#     print("give patient 500mg")
+# elif weight < 40:
+#     print("give patient 750mg")
+# else:
+#     print("give patient 1000mg")
+# 
+
+
+
+# logic operators
+# a = 3 < 2
+# b = 3 <= 3
+# c = 4 > 2
+# d = 5 >= 4
+# e = 5 == 5
+# f = 5 != 4
+# g = 3 > 2 and 2 > 1
+# h = 3 > 3 or 3 < 2
+# i = not False == True
+# j = not i
+# p = "21" < "3" # 'lexicografisk ordning'
+
+
+
+# While loops
+# 
+# loop = True
+# n = 0
+# while loop:
+#     n = n + 2
+#     print(f"Still True {n}")
+#     if n > 10:
+#         loop = False
+
+
+ds = {"a": "aval", "b": "bval", "c": "cval"}
+
+# key, value
+# items
+# values
+for k, v in ds.items:
+    print(k, v)
