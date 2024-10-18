eca516f77744ba354e227a2f03e107bed3c29529
Ludwilton <ludwig.carlegrund@gmail.com>
2024-10-18 22:32:02 +0200

    added report, updates labb3.py

--- Labs/labb3.py
+++ Labs/labb3.py
@@ -23,10 +23,10 @@ def get_data():
 
 
 
-def classify_data(data=get_data(), k=get_default_k(), m=get_default_m()):
+def classify_data_points(data=get_data(), k=get_default_k(), m=get_default_m()):
     classified_points = []
     count_1 = 0
-    count_2 = 0
+    count_0 = 0
 
     for row in data:
         x, y = row
@@ -36,16 +36,18 @@ def classify_data(data=get_data(), k=get_default_k(), m=get_default_m()):
             count_1 += 1
         else:
             label = 0
-            count_2 += 1
+            count_0 += 1
         
         classified_points.append([x,y,label])
-    print(count_1)
-    print(count_2)
+
+    print(f"Data för linje med lutning: {k}, skärningspunkt {m}")
+    print(f"Antal punkter till höger om linjen: {count_1}")
+    print(f"Antal punkter till vänster om linjen: {count_0}")
     return classified_points
 
 
-def write_classified_labels(k = get_default_k(), m = get_default_m()):
-    classified_data = classify_points(k=k, m=m)
+def write_classified_data_csv(k = get_default_k(), m = get_default_m()):
+    classified_data = classify_data_points(k=k, m=m)
     classified_data = np.array(classified_data, dtype=float)
     print(classified_data)
     path = os.path.dirname(__file__) + "/labelled_data.csv"
@@ -58,39 +60,28 @@ def write_classified_labels(k = get_default_k(), m = get_default_m()):
     print(f"data classified and saved to {w_file}")
 
 
-def plot_line(data=get_data(), k=get_default_k(), m=get_default_m()):
-    x = data[:, 0]
-    y = data[:, 1]
-    y_line = k * x + m
-    plt.scatter(x, y,)
-    plt.plot(x, y_line, color="red", label="y=kx+m")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-
 def plot_classified_points(k = get_default_k(), m = get_default_m(), axes=None, color="red", label="separator line"):
-    data = classify_points(k=k, m=m)
+    data = classify_data_points(k=k, m=m)
     data = np.array(data, dtype=float)
     x = data[:, 0]
     y = data[:, 1]
     l = data[:, 2]
     y_line = k * x + m
 
-    axes.scatter(x[l == 0], y[l==0], color="yellow")
-    axes.scatter(x[l == 1], y[l==1], color="blue")
+    axes.scatter(x[l == 0], y[l==0], color="gold")
+    axes.scatter(x[l == 1], y[l==1], color="cyan")
+    axes.set_ylabel("y")
+    axes.set_xlabel("x")
     axes.plot(x, y_line, color=color, label=label)
     axes.set(ylim = (-5, 5), xlim = (-5, 5))
 
 
 if __name__ == "__main__":
     axes = plt.axes()
-    plot_classified_points(axes=axes)
+    plot_classified_points(axes=axes) # defaultvärden
     plot_classified_points(k=800, m=-120, axes=axes)
     plot_classified_points(k=-0.489, axes=axes)
     plot_classified_points(k=-2, m=0.16, axes=axes)
     plt.grid()
     plt.show()
-    # write_classified_labels()
+    # write_classified_data_csv()
